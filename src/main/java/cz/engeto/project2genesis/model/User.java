dfdb1a086f19ffeb31d89771b0621c8f3a7ca624
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-15 12:59:12 +0200

    Basic setup of class

---
+++ src/main/java/cz/engeto/project2genesis/model/User.java
@@ -0,0 +1,50 @@
+package cz.engeto.project2genesis.model;
+
+import java.util.UUID;
+
+public class User {
+
+    private String name;
+    private String surname;
+    private String personID;
+    private UUID uuid;
+
+    public User(String name, String surname, String personID, UUID uuid) {
+        this.name = name;
+        this.surname = surname;
+        this.personID = personID;
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(String personID) {
+        this.personID = personID;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+}
