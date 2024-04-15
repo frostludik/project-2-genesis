694166b78ac91e863f439201466c5f386b3a04ff
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-15 10:38:50 +0200

    Initial commit

---
+++ src/main/java/cz/engeto/project2genesis/Project2GenesisApplication.java
@@ -0,0 +1,13 @@
+package cz.engeto.project2genesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2GenesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2GenesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/engeto/project2genesis/Project2GenesisApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.engeto.project2genesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2GenesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
