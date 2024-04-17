c84dea940a922dfe40b9af80db03454d49f0748c
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-17 11:13:11 +0200

    added no-argument constructor

--- src/main/java/cz/engeto/project2genesis/model/User.java
+++ src/main/java/cz/engeto/project2genesis/model/User.java
@@ -27,6 +27,9 @@ public class User {
         this.surname = surname;
     }
 
+    public User() {
+    }
+
     public long getId() { return id;}
 
     public void setId(Long id) { this.id = id;}
