e4e007ad62950e1b3c8bfca870f99fca746956b6
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-17 11:12:50 +0200

    Removed unused Standardized strings

--- src/main/java/cz/engeto/project2genesis/util/Settings.java
+++ src/main/java/cz/engeto/project2genesis/util/Settings.java
@@ -16,10 +16,9 @@ public class Settings {
         return resourceLoader.getResource("classpath:dataPersonId.txt");
     }
 
-    public static final String USER_CREATED = "User created successfully.";
+
     public static final String USER_NOT_CREATED = "User was not created.";
-    public static final String USER_NOT_FOUND = "Cannot find User with id = ";
-    public static final String USER_UPDATED = "User updated successfully.";
+    public static final String USER_NOT_FOUND = "Cannot find User. ID = ";
     public static final String USER_NOT_UPDATED = "User was not updated.";
     public static final String USER_DELETED = "User deleted successfully.";
 
