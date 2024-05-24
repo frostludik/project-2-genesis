c7e04419c8fdedb051800e559c725d361093e15f
Ludek Mraz <ludek.mraz@centrum.cz>
2024-05-24 14:34:01 +0200

    added variable requiredPersonIdLength replacing magic number in validatePersonID method

--- src/main/java/cz/engeto/project2genesis/util/ValidationUtils.java
+++ src/main/java/cz/engeto/project2genesis/util/ValidationUtils.java
@@ -3,11 +3,12 @@ package cz.engeto.project2genesis.util;
 public class ValidationUtils {
 
     public static void validatePersonID(String personID) {
+        int requiredPersonIdLength = 12;
         if (personID == null) {
             throw new IllegalArgumentException("personID cannot be null");
         }
-        if (personID.length() != 12) {
-            throw new IllegalArgumentException("personID must be exactly 12 characters long");
+        if (personID.length() != requiredPersonIdLength) {
+            throw new IllegalArgumentException("personID must be exactly " + requiredPersonIdLength + " characters long");
         }
     }
 
